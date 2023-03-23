a00636549bc3b41251dcb601a242ccf54a605495
ggumtle_skull <huiseong8442@gmail.com>
2023-03-23 13:48:59 +0900

    GUI 만들기 테스트 및 연습 (btncmd 함수가 사용되지 않음) - 박희성

---
+++ GUI_test_park.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+def btncmd():
+    global txt
+    print(txt.get("1.0", END))
+    txt.delete("1.0", END)
+
+root = Tk()
+root.title("GUI TEST")  # GUI 제목
+root.geometry("540x380")  # 가로 * 세로
+
+txt = Text(root, width=20, height=3)
+txt.pack()
+
+btn = Button(root, text="click", command=btncmd())
+btn.pack()
+
+root.mainloop()
